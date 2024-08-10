8e6a3e2caf63c77520d56e633b4df07e500d25bc
nemonuri <nemonuri2@gmail.com>
2024-08-10 16:45:25 +0900

    add test - 그리고, dotnet cli 로 add reference 를 할 수 있구나!

---
+++ tests/Nemonuri.AdhocTables.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Nemonuri.AdhocTables.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //Model
+        
+
+        //Act
+
+        //Assert
+    }
+}
