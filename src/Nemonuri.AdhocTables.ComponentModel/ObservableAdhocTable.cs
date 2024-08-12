a07432667a062bb8c21f850c3684c28de028ecab
nemonuri <nemonuri2@gmail.com>
2024-08-12 13:10:48 +0900

    Make Nemonuri.AdhocTables.ComponentModel project

---
+++ src/Nemonuri.AdhocTables.ComponentModel/ObservableAdhocTable.cs
@@ -0,0 +1,14 @@
+﻿namespace Nemonuri.AdhocTables.ComponentModel;
+
+public class ObservableAdhocTable : ObservableObject
+{
+    public ObservableAdhocTable(AdhocTableBuilder adhocTableBuilder):base()
+    {
+        Guard.IsNotNull(adhocTableBuilder);
+        AdhocTable = adhocTableBuilder.Build();
+    }
+
+    public AdhocTable AdhocTable {get;}
+
+
+}
---
+++ src/Nemonuri.AdhocTables.ComponentModel/_Global/globals.cs
@@ -0,0 +1,5 @@
+global using System.Globalization;
+global using System.Diagnostics.CodeAnalysis;
+global using CommunityToolkit.Diagnostics;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using Nemonuri.AdhocTables;
